72059de0c355d8b5f0240055790e06537a3611f6
Moha-hasa <hu62sa62@gmail.com>
2024-02-26 11:43:07 +0530

    [ADDED]: Husain Employee wage Computation

---
+++ src/Employee/EmployeeWageComp.java
@@ -0,0 +1,16 @@
+package Employee;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+		int attendance = 0;
+		if(attendance == 0) {
+			System.out.println("Employee is absent");
+		}
+		else {
+			System.out.println("Employee is present");
+		}
+
+	}
+
+}
