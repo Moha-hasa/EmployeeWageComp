e26573512191db22916a44470fd5daec097a4d65
Moha-hasa <hu62sa62@gmail.com>
2024-02-26 12:03:27 +0530

    [MODIFIED]:Husain - UC 1 Completed for Employee Attendace using Random class

--- src/Employee/EmployeeWageComp.java
+++ src/Employee/EmployeeWageComp.java
@@ -1,9 +1,14 @@
 package Employee;
 
+import java.util.Random;
+
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
 		int attendance = 0;
+		System.out.println("Welcome to Employee Wage computation");
+		Random random = new Random();
+		attendance = random.nextInt(9) % 2; 
 		if(attendance == 0) {
 			System.out.println("Employee is absent");
 		}
