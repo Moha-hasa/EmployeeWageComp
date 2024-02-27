7960437c194aa536f5e4c72ac5116b578576c1ac
Moha-hasa <hu62sa62@gmail.com>
2024-02-27 11:34:27 +0530

    [MODIFIED]:Husain - Added Employee Type Check Functionality

--- src/Employee/EmployeeWageComp.java
+++ src/Employee/EmployeeWageComp.java
@@ -5,10 +5,14 @@ import java.util.Random;
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		int attendance = 0;
-		int dailywage = 0;
+		int attendance;
+		int dailywage;
+		int employee_type;
 		final int WAGE_PER_HR = 20;
 		final int FULL_DAY_HR = 8; 
+		final int FULL_TIME_HR = 8;
+		final int PART_TIME_HR = 4;
+		
 		System.out.println("Welcome to Employee Wage computation");
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2; 
@@ -17,7 +21,19 @@ public class EmployeeWageComp {
 		}
 		else {
 			System.out.println("Employee is present");
-			dailywage = WAGE_PER_HR * FULL_DAY_HR;
+				employee_type = random.nextInt(9) % 2;
+			
+			if(employee_type == 0){
+				
+				System.out.println("Employee is part time");
+				dailywage = WAGE_PER_HR * PART_TIME_HR;
+				
+			}
+			else {
+				System.out.println("Employee is full time");
+				dailywage = WAGE_PER_HR * FULL_DAY_HR;
+			}
+			
 			System.out.println("Employee wage = "+dailywage);
 		}
 
